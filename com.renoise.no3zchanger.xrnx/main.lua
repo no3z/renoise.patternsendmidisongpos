743cd0e28687105e6cd8eb5283413f9f153268fe
no3productionz@gmail.com <no3produtionz@gmail.com>
2014-04-16 18:58:37 +0200

    Updated main.lua
    Correct calculation of current measure. Fixes, comments.

--- com.renoise.no3zchanger.xrnx/main.lua
+++ com.renoise.no3zchanger.xrnx/main.lua
@@ -1,30 +1,32 @@
-prev_pos = 0
-t = 0
-device_name = ""
-
+--Initialize midi output to the first device
 
 local outputs = renoise.Midi.available_output_devices()
 if not table.is_empty(outputs) then 
-  device_name = outputs[1]  
-  midi_device = renoise.Midi.create_output_device(device_name)   
-  midi_device:send {0x90, 0x10, 0x7F}  
-  midi_device:send {0xF0, 0x7F, 0x00, 0x06, 0x02, 0xF7}  
+  local device_name = outputs[1]  
+  midi_device = renoise.Midi.create_output_device(device_name)    
 end
+
+-- Function to call in the idle handler
+-- Calculate current measure, taking into account pattern 
+-- with lenght=64. Send it via SongPos everytime the value changes.
+
+function idle_handler()
+ local prev_pos = 0
+ local t = renoise.song().transport.playback_pos
+ local measure = (((t.sequence-1) * 4) + (t.line/16))+1
+ if prev_pos ~= measure then  
+  midi_device:send {0xF2, measure}      
+  prev_pos = t.sequence
+ end
+end
+
+-- Register the idle function 
+
+renoise.tool().app_idle_observable:add_notifier(idle_handler)
+
+-- Someone, somewhere should call this. 
 
 function stopSend()
   midi_device:close()  
 end
-
-function idle_handler()
- t = renoise.song().transport.playback_pos
- if prev_pos ~= t.sequence then  
-  midi_device:send {0xF2, t.sequence-1}      
-  prev_pos = t.sequence
- end
- 
-end
- 
-renoise.tool().app_idle_observable:add_notifier(idle_handler)
-
-  
 
